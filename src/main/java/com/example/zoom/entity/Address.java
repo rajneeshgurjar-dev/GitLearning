471a2267a300caa322e17afdf53357e99df3ded8
rajneeshgurjar-dev <rajneesh.gurjar@innogent.in>
2025-12-29 23:13:42 +0530

    address entity updated

--- src/main/java/com/example/zoom/entity/Address.java
+++ src/main/java/com/example/zoom/entity/Address.java
@@ -1,4 +1,20 @@
 package com.example.zoom.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Address {
+    @Id
+    private String id;
+    private String street;
+    private String city;
+    private String state;
+
 }
